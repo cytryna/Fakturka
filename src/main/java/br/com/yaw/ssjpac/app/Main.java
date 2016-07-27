28a799d761f335c531cc30fc0bff852aef47d45d
rwichrowski <rwichrowski@efigence.com>
2016-07-27 07:50:26 +0200

    Zmiany łukasza

--- src/main/java/br/com/yaw/ssjpac/app/Main.java
+++ src/main/java/br/com/yaw/ssjpac/app/Main.java
@@ -3,7 +3,12 @@ package br.com.yaw.ssjpac.app;
 
 import java.util.Locale;
 
+import br.com.yaw.ssjpac.controller.BuscarMercadoriaController;
+import br.com.yaw.ssjpac.controller.ListaMercadoriaController;
+import br.com.yaw.ssjpac.ui.BuscaMercadoriaFrame;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import javax.swing.*;
 
 /**
  * Ponto de entrada da aplicação.
@@ -14,7 +19,7 @@ public class Main {
 	
 	public static void main(String[] args) {
 		Locale.setDefault(new Locale("pl","PL"));
-		new ClassPathXmlApplicationContext("META-INF/spring-config.xml");
+		ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("META-INF/spring-config.xml");
 	}
 
 }
