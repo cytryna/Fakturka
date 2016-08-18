3816d5d35442388d4f532c7a0787c2df3fac14bf
rwichrowski <rwichrowski@efigence.com>
2016-08-18 07:00:13 +0200

    Pierwsze wydruki - tableka

--- src/main/java/diligentia/iText/HelloWorld.java
+++ src/main/java/diligentia/iText/HelloWorld.java
@@ -6,7 +6,9 @@ import java.io.IOException;
 
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
-import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.Phrase;
+import com.itextpdf.text.Rectangle;
+import com.itextpdf.text.pdf.PdfPCell;
 import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfWriter;
 
@@ -16,20 +18,79 @@ public class HelloWorld {
     public static final String RESULT
             = "/home/rwichrowski/fakturka/hello.pdf";
 
+    /**
+     * Main method.
+     * @param    args    no arguments needed
+     * @throws DocumentException
+     * @throws IOException
+     */
     public static void main(String[] args)
             throws DocumentException, IOException {
-        new HelloWorld().createPdf(RESULT);
-    }
-
-    public void createPdf(String dest) throws IOException, DocumentException {
+        // step 1
         Document document = new Document();
-        PdfWriter.getInstance(document, new FileOutputStream(dest));
+        // step 2
+        PdfWriter.getInstance(document, new FileOutputStream(RESULT));
+        // step 3
         document.open();
-        PdfPTable table = new PdfPTable(8);
-        for(int aw = 0; aw < 16; aw++){
-            table.addCell("hi");
-        }
+        // step 4
+        PdfPTable table = new PdfPTable(2);
+        table.setWidthPercentage(100);
+        Phrase p = new Phrase(
+                "Dr. iText or: How I Learned to Stop Worrying " +
+                        "and Love the Portable Document Format.");
+        PdfPCell cell = new PdfPCell(p);
+        cell.setBorder(Rectangle.NO_BORDER);
+        table.addCell("default leading / spacing");
+        table.addCell(cell);
+        table.addCell("absolute leading: 20");
+        cell.setLeading(20f, 0f);
+        table.addCell(cell);
+        table.addCell("absolute leading: 3; relative leading: 1.2");
+        cell.setLeading(3f, 1.2f);
+        table.addCell(cell);
+        table.addCell("absolute leading: 0; relative leading: 1.2");
+        cell.setLeading(0f, 1.2f);
+        table.addCell(cell);
+        table.addCell("no leading at all");
+        cell.setLeading(0f, 0f);
+        table.addCell(cell);
+        cell = new PdfPCell(new Phrase(
+                "Dr. iText or: How I Learned to Stop Worrying and Love PDF"));
+        table.addCell("padding 10");
+        cell.setPadding(10);
+        table.addCell(cell);
+        table.addCell("padding 0");
+        cell.setPadding(0);
+        table.addCell(cell);
+        table.addCell("different padding for left, right, top and bottom");
+        cell.setPaddingLeft(20);
+        cell.setPaddingRight(50);
+        cell.setPaddingTop(0);
+        cell.setPaddingBottom(5);
+        table.addCell(cell);
+        p = new Phrase("iText in Action Second Edition");
+        table.getDefaultCell().setPadding(2);
+        table.getDefaultCell().setUseAscender(false);
+        table.getDefaultCell().setUseDescender(false);
+        table.addCell("padding 2; no ascender, no descender");
+        table.addCell(p);
+        table.getDefaultCell().setUseAscender(true);
+        table.getDefaultCell().setUseDescender(false);
+        table.addCell("padding 2; ascender, no descender");
+        table.addCell(p);
+        table.getDefaultCell().setUseAscender(false);
+        table.getDefaultCell().setUseDescender(true);
+        table.addCell("padding 2; descender, no ascender");
+        table.addCell(p);
+        table.getDefaultCell().setUseAscender(true);
+        table.getDefaultCell().setUseDescender(true);
+        table.addCell("padding 2; ascender and descender");
+        cell.setPadding(2);
+        cell.setUseAscender(true);
+        cell.setUseDescender(true);
+        table.addCell(p);
         document.add(table);
+        // step 5
         document.close();
     }
 }
