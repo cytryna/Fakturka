41f046afed722e237275f7100db62e563026f922
rwichrowski <rwichrowski@efigence.com>
2016-08-30 07:01:36 +0200

    Pomysł z Forum niestety jest problem z Lambdą

--- src/main/java/diligentia/view/MainFrame.java
+++ src/main/java/diligentia/view/MainFrame.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Component;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionListener;
 
 @Component
 public class MainFrame extends JFrame {
@@ -14,6 +15,7 @@ public class MainFrame extends JFrame {
 
     public MainFrame() {
         init();
+
     }
 
     private void init() {
@@ -25,12 +27,30 @@ public class MainFrame extends JFrame {
         contentPanel.setBorder(BorderFactory.createLineBorder(Color.GRAY));//TODO-rwichrowski jakiś properties z kolorami
         contentPanel.setLayout(new CardLayout());
 
-        menuPanel = new MenuPanel(contentPanel);
+//        menuPanel = new MenuPanel(contentPanel);
+        menuPanel = new JPanel();
         menuPanel.setVisible(true);
 
         mainPanel.setVisible(true);
         mainPanel.setLayout(new BorderLayout());
         mainPanel.add(menuPanel, BorderLayout.NORTH);
         mainPanel.add(contentPanel, BorderLayout.CENTER);
+
+        createButton("First", e -> swichPanel(new JLabel("first")));
+//        createButton("Second", e -> swichPanel(new JLabel("second")));
     }
+
+
+    public void createButton(String buttonName, ActionListener listener)
+    {
+        JButton jButton = new JButton(buttonName);
+        jButton.addActionListener(listener);
+        menuPanel.add(jButton);
+    }
+
+
+    private void swichPanel(JComponent component) {
+        contentPanel.add(component);
+    }
+
 }
