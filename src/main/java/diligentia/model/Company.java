a82daf78fda2ce9cbdff5f9e98f4f06a0d9db157
rwichrowski <rwichrowski@efigence.com>
2016-09-28 06:56:27 +0200

    20 minut GUI

--- src/main/java/diligentia/model/Company.java
+++ src/main/java/diligentia/model/Company.java
@@ -14,6 +14,49 @@ public class Company { //TODO-rwichrowski może zrobić to zbstrakcyjną i dodac
 
     private String name;
 
-    private String address;
+    private String postCode;
 
+    private String city;
+
+    private String street;
+
+    public String getTaxIdentificationNumber() {
+        return taxIdentificationNumber;
+    }
+
+    public void setTaxIdentificationNumber(String taxIdentificationNumber) {
+        this.taxIdentificationNumber = taxIdentificationNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPostCode() {
+        return postCode;
+    }
+
+    public void setPostCode(String postCode) {
+        this.postCode = postCode;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
 }
--- src/main/java/diligentia/view/IssueInvoiceView.java
+++ src/main/java/diligentia/view/IssueInvoiceView.java
@@ -1,39 +1,75 @@
 package diligentia.view;
 
+import diligentia.model.Company;
 import diligentia.util.GridBagConstraintsBuilder;
 
 import javax.swing.*;
 import java.awt.*;
 
+import static diligentia.util.GridBagConstraintsBuilder.constraints;
+import static diligentia.util.GridBagConstraintsBuilder.fillDefaults;
+
 public class IssueInvoiceView extends JPanel {
 
-    public IssueInvoiceView() {
+	public IssueInvoiceView() {
 
-        init();
-        setBorder(BorderFactory.createLineBorder(Color.magenta));
+		init();
+		setBorder(BorderFactory.createLineBorder(Color.magenta));
+	}
+
+	private void init() {
+		Company sellerCompany = new Company();
+		sellerCompany.setTaxIdentificationNumber("NIP 775-000-78-54");
+		sellerCompany.setName("wich-mot edward Wichrowski");
+		sellerCompany.setPostCode("09-500");
+		sellerCompany.setCity("Gostynin");
+		sellerCompany.setStreet("ul. Chopina 4");
+
+		Company customerCompany = new Company();
+		customerCompany.setTaxIdentificationNumber("NIP 465-884-55-22");
+		customerCompany.setName("Firma krzak");
+		customerCompany.setPostCode("88-888");
+		customerCompany.setCity("Kopydłów");
+		customerCompany.setStreet("ul. Zagrobelna 4423");
+
+		setLayout(new GridBagLayout());
+		add(new JTextField("Faktura Vat"), constraints().withPosition(0, 0).build());
+		add(new JTextField("15/2016"), constraints().withPosition(0, 1).build());
+		add(new JTextField("Warszawa 2015-12-29"), constraints().withPosition(0, 2).build());
+		add(createCompanyPanel("Sprzedwaca", sellerCompany),
+			fillDefaults().withPosition(0, 3).build());
+		add(createCompanyPanel("Nabywca", customerCompany),
+			fillDefaults().withPosition(1, 3).build());
+
+
+        add(createArticleTable(),
+                fillDefaults().withPosition(0, 4).build());
+
+	}
+
+    private Component createArticleTable() {
+        return new JLabel("tabelka artykułów");
     }
 
-    private void init() {
-        setLayout(new GridBagLayout());
-        add(new JTextField("Faktura Vat"), GridBagConstraintsBuilder.constraints().build());
-        add(new JTextField("15/2016"), GridBagConstraintsBuilder.constraints().build());
-        add(new JTextField("Warszawa 2015-12-29"), GridBagConstraintsBuilder.constraints().build());
-        add(new JLabel("Sprzedwaca"), GridBagConstraintsBuilder.constraints().build());
-        add(new JTextField("NIP 775-000-78-54"), GridBagConstraintsBuilder.constraints().build());
-        add(new JTextField("wich-mot edward Wichrowski"), GridBagConstraintsBuilder.constraints().build());
-        add(new JTextField("09-500 Gostynin"), GridBagConstraintsBuilder.constraints().build());
-        add(new JTextField("ul. Chopina 4"), GridBagConstraintsBuilder.constraints().build());
-        add(new JLabel("Nabywca"), GridBagConstraintsBuilder.constraints().build());
-        add(new JTextField("Firma krzak"), GridBagConstraintsBuilder.constraints().build());
-        add(new JTextField("88-888 Kopydłów"), GridBagConstraintsBuilder.constraints().build());
-        add(new JTextField("ul. Zagrobelna 4423"), GridBagConstraintsBuilder.constraints().build());
-        add(new JTextField("NIP 465-884-55-22"), GridBagConstraintsBuilder.constraints().build());
+    private Component createCompanyPanel(String title, Company company) {
+		JPanel jPanel = new JPanel(new GridBagLayout());
+		jPanel.setBorder(BorderFactory.createLineBorder(Color.GRAY));
+		int y = 0;
+		jPanel.add(new JLabel(title),
+			constraints().withPosition(0, y++).withAnchor(GridBagConstraints.WEST).build());
+		jPanel.add(new JTextField(company.getTaxIdentificationNumber()),
+			constraints().withPosition(0, y++).withAnchor(GridBagConstraints.WEST).build());
+		jPanel.add(new JTextField(company.getName()),
+			constraints().withPosition(0, y++).withAnchor(GridBagConstraints.WEST).build());
+		jPanel.add(new JTextField(company.getPostCode() + " " + company.getCity()),
+			constraints().withPosition(0, y++).withAnchor(GridBagConstraints.WEST).build());
+		jPanel.add(new JTextField(company.getStreet()),
+			constraints().withPosition(0, y++).withAnchor(GridBagConstraints.WEST).build());
+		return jPanel;
+	}
 
-
-    }
-
-//    https://examples.javacodegeeks.com/core-java/java-swing-mvc-example/
-//    lub inne
-//    https://www.google.pl/webhp?sourceid=chrome-instant&ion=1&espv=2&ie=UTF-8#q=java+swing+mvc+example+code+
-//    w java2s jest kiepski przykłąd
+	// https://examples.javacodegeeks.com/core-java/java-swing-mvc-example/
+	// lub inne
+	// https://www.google.pl/webhp?sourceid=chrome-instant&ion=1&espv=2&ie=UTF-8#q=java+swing+mvc+example+code+
+	// w java2s jest kiepski przykłąd
 }
