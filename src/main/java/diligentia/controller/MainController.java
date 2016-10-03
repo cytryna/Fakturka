609b41ddea4666d34f845ebd30c69202217b5839
rwichrowski <rwichrowski@efigence.com>
2016-10-03 06:27:24 +0200

    dane testowe

--- src/main/java/diligentia/controller/MainController.java
+++ src/main/java/diligentia/controller/MainController.java
@@ -2,6 +2,7 @@ package diligentia.controller;
 
 import br.com.yaw.ssjpac.ui.ListArticleFrame;
 import diligentia.dao.ArticleDAO;
+import diligentia.model.Article;
 import diligentia.view.MainFrame;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -9,21 +10,31 @@ import org.springframework.stereotype.Component;
 import javax.annotation.PostConstruct;
 import javax.swing.*;
 import java.awt.*;
+import java.util.*;
+import java.util.List;
 
 @Component
 public class MainController {
 
+    List<Article> articles = new ArrayList<>();
+
+
     @Autowired
     private MainFrame frame;
 
-    @Autowired
-    private ArticleDAO articleDAO;
+//    @Autowired
+//    private ArticleDAO articleDAO;
 
     @PostConstruct
     private void init() {
 
 
-        frame.refreshTable(articleDAO.findAll());
+//        frame.refreshTable(articleDAO.findAll());
+        articles.add(new Article(1, "Silnik", "opis", 3, 3.0, 2));
+        articles.add(new Article(2, "Alternator", "opis", 4, 4.0, 4));
+
+        frame.refreshTable(articles);
+
         frame.setExtendedState(frame.getExtendedState() | JFrame.MAXIMIZED_BOTH);
         frame.setSize(new Dimension(1200, 800));
         frame.setVisible(true);
@@ -32,7 +43,11 @@ public class MainController {
 
 
     private void refreshTable() {
-        this.frame.refreshTable(this.articleDAO.findAll());
+//        this.frame.refreshTable(this.articleDAO.findAll());
+        articles.add(new Article(1, "Silnik", "opis", 3, 3.0, 2));
+        articles.add(new Article(2, "Alternator", "opis", 4, 4.0, 4));
+        frame.refreshTable(articles);
+
     }
 
 }
