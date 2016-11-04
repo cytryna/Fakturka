58bacfc3d0aed0f6f1a2b0e8d108035625c6ba64
rwichrowski <rwichrowski@efigence.com>
2016-11-04 07:00:40 +0100

    Drukowanie

--- src/main/java/diligentia/iText/Printer.java
+++ src/main/java/diligentia/iText/Printer.java
@@ -54,42 +54,42 @@ public class Printer {
 			PdfPTable table = new PdfPTable(2);
 			table.getDefaultCell().setBorder(PdfPCell.NO_BORDER);
 			table.setWidthPercentage(100);
-			Phrase salesman = new Phrase("radek"+model.getSalesman().getTaxIdentificationNumber()+
-				"Wich-mot Edward Wichrowski\n09-500 Goostynin\nul. Szopena 4\n");
-			Phrase customer = new Phrase(
-				"Diligentia Radosław Wichrowski\n04-113 Warszawa\nul. Łukowska 1 m 156\nNIP: 971-060-22-10");
+			Phrase salesman = new Phrase("SPRZEADWACA:\nWich-mot Edward Wichrowski\n09-500 Goostynin\nul. Szopena 4\n");
+			Phrase customer = new Phrase("NABYWCA\nDiligentia Radosław Wichrowski\n04-113 Warszawa\nul. Łukowska 1 m 156\nNIP: 971-060-22-10");
 
 			// PdfPCell emptyCell = new PdfPCell(salesman);
 			PdfPCell emptyCell = new PdfPCell();
-
 			emptyCell.setBorder(Rectangle.NO_BORDER);
-			table.addCell("Faktura VAT");
-			table.addCell("d");
-			table.addCell("15/12/0049");
-			// emptyCell.setLeading(20f, 0f);
+			table.addCell("Wystawiono dnia "+model.getDate()+", "+model.getCity());
 			table.addCell(emptyCell);
-			table.addCell("Oryginał");// TODO-rwichrowski Polskie znaki
+			table.addCell(emptyCell);
+			table.addCell("Faktura VAT nr "+model.getNumber());
+			table.addCell(emptyCell);
+			table.addCell("data sprzedaży "+model.getDate());//TODO-rwichrowski dodać date sprzadaży
+			table.addCell(emptyCell);
+			table.addCell("termin płatności "+model.getDate());//TODO-rwichrowski dodać termin płatności
+//			emptyCell.setLeading(20f, 0f);
+			table.addCell(emptyCell);
 			// emptyCell.setLeading(3f, 1.2f);
 			table.addCell(emptyCell);
-			table.addCell("Warszawa 2015-12-29" + model.getCity());
+
 			// emptyCell.setLeading(0f, 1.2f);
-			table.addCell(emptyCell);
 			table.addCell(salesman);
 			table.addCell(customer);
 
-			emptyCell = new PdfPCell(new Phrase("1"));
-			table.addCell("padding 10");
-			// emptyCell.setPadding(10);
-			table.addCell(emptyCell);
-			table.addCell("padding 0");
-			// emptyCell.setPadding(0);
-			table.addCell(emptyCell);
-			table.addCell("2");
+//			emptyCell = new PdfPCell(new Phrase("1"));
+//			emptyCell.setPadding(10);
+//			table.addCell(emptyCell);
+//			table.addCell(emptyCell);
+//			table.addCell("padding 0");
+//			emptyCell.setPadding(0);
+//			table.addCell(emptyCell);
+//			table.addCell("2");
 			// emptyCell.setPaddingLeft(20);
 			// emptyCell.setPaddingRight(50);
 			// emptyCell.setPaddingTop(0);
 			// emptyCell.setPaddingBottom(5);
-			table.addCell(emptyCell);
+//			table.addCell(emptyCell);
 			// salesman = new Phrase("iText in Action Second Edition");
 			// table.getDefaultCell().setPadding(2);
 			// table.getDefaultCell().setUseAscender(false);
@@ -115,10 +115,10 @@ public class Printer {
 
 			BaseFont times = null;
 
-			times = BaseFont.createFont(BaseFont.TIMES_ROMAN, BaseFont.CP1250, true);
+			times = BaseFont.createFont(BaseFont.HELVETICA, BaseFont.CP1250, true);
 
 			Font t9 = new Font(times, 14);
-			String line = "Śćźół:";
+			String line = "POZYCJE FAKTURY:";
 			document.add(new Paragraph(line, t9));
 
 			PdfPTable productTabe = new PdfPTable(2);
